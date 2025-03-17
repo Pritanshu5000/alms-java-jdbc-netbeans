6a9c3404c928d1921ee96ec646a9d730710e502f
Pritanshu5000 <167696713+Pritanshu5000@users.noreply.github.com>
2025-03-17 22:11:50 +0530

    Update AdvanceLibraryManagementSystem.java

--- src/com/cgu/advance_library_management_system/AdvanceLibraryManagementSystem.java
+++ src/com/cgu/advance_library_management_system/AdvanceLibraryManagementSystem.java
@@ -4,7 +4,6 @@ package com.cgu.advance_library_management_system;
 public class AdvanceLibraryManagementSystem {
 
     public static void main(String[] args) {
-        
+        new Login().setVisible(true);
     }
-    
 }
