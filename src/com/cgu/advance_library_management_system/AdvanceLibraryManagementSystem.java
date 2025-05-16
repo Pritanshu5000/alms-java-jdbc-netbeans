298a2e491509f103b4cd28a8448d9a9a6862735d
Pritanshu Kumar Mallick <167696713+Pritanshu5000@users.noreply.github.com>
2025-05-17 02:50:54 +0530

    Delete src directory

--- src/com/cgu/advance_library_management_system/AdvanceLibraryManagementSystem.java
+++
@@ -1,9 +0,0 @@
-
-package com.cgu.advance_library_management_system;
-
-public class AdvanceLibraryManagementSystem {
-
-    public static void main(String[] args) {
-        new Login().setVisible(true);
-    }
-}
